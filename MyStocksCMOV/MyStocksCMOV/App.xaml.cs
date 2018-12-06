ff3dfa59c049eed475ee00e608a33a65c23b68c4
Tiago Grosso <tiagogrosso99@gmail.com>
2018-12-06 10:39:13 +0000

    Initial Template

--- MyStocksCMOV/MyStocksCMOV/App.xaml.cs
+++ MyStocksCMOV/MyStocksCMOV/App.xaml.cs
@@ -1,25 +1,15 @@
 ﻿using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
-using MyStocksCMOV.Services;
 using MyStocksCMOV.Views;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 namespace MyStocksCMOV {
 				public partial class App : Application {
-								//TODO: Replace with *.azurewebsites.net url after deploying backend to Azure
-								public static string AzureBackendUrl = "http://localhost:5000";
-								public static bool UseMockDataStore = true;
+
 
 								public App()
 								{
-												InitializeComponent();
-
-												if (UseMockDataStore)
-																DependencyService.Register<MockDataStore>();
-												else
-																DependencyService.Register<AzureDataStore>();
-
 												MainPage = new MainPage();
 								}
 
--- MyStocksCMOV/MyStocksCMOV/Views/MainPage.xaml.cs
+++ MyStocksCMOV/MyStocksCMOV/Views/MainPage.xaml.cs
@@ -1,14 +1,17 @@
 ﻿using System;
 
 using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
 using Xamarin.Forms.Xaml;
 
 namespace MyStocksCMOV.Views {
 				[XamlCompilation(XamlCompilationOptions.Compile)]
-				public partial class MainPage : TabbedPage {
+				public partial class MainPage : Xamarin.Forms.TabbedPage {
 								public MainPage()
 								{
 												InitializeComponent();
+
+												On<Xamarin.Forms.PlatformConfiguration.Android>().SetToolbarPlacement(ToolbarPlacement.Bottom);
 								}
 				}
 }
