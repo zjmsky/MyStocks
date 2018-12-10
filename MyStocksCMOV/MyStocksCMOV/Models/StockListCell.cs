f3445ea8be9110b51a294be6660f9198fffacdbb
zecst19 <zecst19@gmail.com>
2018-12-10 15:41:11 +0000

    list

---
+++ MyStocksCMOV/MyStocksCMOV/Models/StockListCell.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyStocksCMOV.Models
+{
+    class StockListCell
+    {
+        public string CompanyCode { get; }
+        public string CompanyName { get; }
+        public string CompanyStockValue { get; }
+        public string CompanyBalance { get; }
+        public bool On { get; set; }
+
+        public StockListCell(string companyCode, string companyName, float stockValue, float balance, bool on)
+        {
+            this.CompanyName = companyName;
+            this.CompanyCode = companyCode;
+            this.CompanyStockValue = stockValue.ToString("R");
+            this.CompanyBalance = balance.ToString("R");
+            this.On = on;
+        }
+    }
+}
